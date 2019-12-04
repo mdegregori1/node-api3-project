1785b25f22272b70165798e4d76af4d79ee969a2
Mauricio Degregori <degregori2@gmail.com>
2019-12-04 15:57:48 -0600

    logger working

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
+
+
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', logger,  (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl} at ${ new Date().toISOString()}`)
+  next();
+}
 
+server.use(express.json());
+server.use(logger)
 module.exports = server;

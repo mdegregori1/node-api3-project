d35026285e3138bd363f2b2349c8d656a3703faa
Mauricio Degregori <degregori2@gmail.com>
2019-12-04 15:09:02 -0600

    sanity check passed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code away!
+const server = require("./server");
+
+const port = 7000;
+server.listen(7000, () => {
+  console.log(`\n* The server is currently running on http://localhost:${port}*\n`);
+});
+
+
+
